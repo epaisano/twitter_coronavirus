44a4f1fb95a033ea7933b15efc8bc9af9e16c097
Emma Munoz <epaisano24@cmc.edu>
2023-04-28 15:32:18 -0700

    spacing out x

--- src/visualize.py
+++ src/visualize.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import matplotlib
+matplotlib.use('Agg')
 
 # command line args
 import argparse
@@ -12,6 +14,7 @@ args = parser.parse_args()
 import os
 import json
 from collections import Counter,defaultdict
+from operator import add
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
@@ -33,6 +36,15 @@ for k,v in items:
 # making bar chart
 cat = [cat for cat,val in items]
 val = [val for cat,val in items]
-fig = plt.figure()
-plt.bar(cat,val)
-plt.show()
+x = list(range(len(cat)))
+width = 0.25
+widthlis = [0.25 for x in list(range(len(cat)))]
+
+plt.rcdefaults()
+fig, ax = plt.subplots()
+ax.barh(cat,val, align='center')
+ax.set_xlabel('Frequency of #coronavirus Usage')
+ax.set_title('Frequency of #coronavirus Usage by Language')
+ax.set_xticks(list(map(add, x, widthlis)), cat)
+out_png = 'out_file.png'
+plt.savefig(out_png, dpi=150)
